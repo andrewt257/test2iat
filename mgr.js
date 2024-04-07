54b66e42c024a22d1411184d9b276b3b23bd926c
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-04-07 15:31:35 -0700

    add demographics

--- mgr.js
+++ mgr.js
@@ -159,6 +159,7 @@ define(['managerAPI',
             mixer:'random',
             data:[
                 {inherit: 'explicits'},
+                {inherit: 'demographics'},
                 // force the instructions to preceed the iat
                 {
                     mixer: 'wrapper',
