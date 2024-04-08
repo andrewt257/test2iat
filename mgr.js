3fcf6b508c4c9a1daa313cf6cf2452d4ae409945
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-04-07 19:38:15 -0700

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -162,7 +162,7 @@ define(['managerAPI',
         
         {inherit: 'intro'},
         {
-            mixer:'random',
+            mixer:'wrapper',
             data:[
                 {inherit: 'demographics'},
                 {inherit: 'explicits'},
