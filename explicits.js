ff541bc181dd09f0c6c9a34f604977cdb2c7b3dc
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-04-07 20:03:35 -0700

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -5,6 +5,8 @@ define(['questAPI'], function(Quest){
     /**
 	* Page prototype
 	*/
+    API.addCurrent({baseURL:'/images/'});
+
     API.addPagesSet('basicPage',{
         noSubmit:false, //Change to true if you don't want to show the submit button.
         header: 'Questionnaire',
@@ -73,21 +75,21 @@ define(['questAPI'], function(Quest){
             {text:'I moderately prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:2},
             {text:'I strongly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:1}
         ],
-        img: 'http://via.placeholder.com/350x150'
+        data: {imageName: bf1_nc.jpg},
     });
 	
     API.addQuestionsSet('thermBlack',{
         inherit : 'therm',
         name: 'Tblack_0to10',
         stem: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-        img: 'http://via.placeholder.com/350x150'
+        data: {imageName: wf1_nc.jpg},
     });
 
     API.addQuestionsSet('thermWhite',{
         inherit : 'therm',
         name: 'Twhite_0to10',
         stem: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-        img: 'http://via.placeholder.com/350x150'
+        data: {imageName: bm3_nc.jpg},
     });
 
     API.addSequence([
