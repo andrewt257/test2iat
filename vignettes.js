af4ed8ba95aaeaf2ca54e4be424e49b0c393f8c7
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-05-31 21:17:42 -0700

    Update vignettes.js

--- vignettes.js
+++ vignettes.js
@@ -94,14 +94,14 @@ define(['questAPI'], function(Quest){
 
     API.addQuestionsSet('blackFemale1',{
         inherit : 'therm',
-        name: 'thermWhite1',
+        name: 'blackFemale1',
         stem: ['Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?','<img class="img-responsive" src="<%=current.baseURL%>bf1_nc.jpg">']
 
     });
 
     API.addQuestionsSet('whiteFemale1',{
         inherit : 'therm',
-        name: 'thermWhite2',
+        name: 'whiteFemale1',
         stem: ['Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?','<img class="img-responsive" src="<%=current.baseURL%>wf1_nc.jpg">']
 
     });
