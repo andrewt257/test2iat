1f49caacc2f71d95c5aeb519839f4d32eeb679ba
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-06-01 18:18:52 -0700

    Update demographics.js

--- demographics.js
+++ demographics.js
@@ -117,11 +117,9 @@ define(['questAPI','underscore'], function(Quest,_){
     });
 
     API.addQuestionsSet('birthYear',{
-        inherit: 'singleChoicedrop',
+        inherit: 'text',
         name: 'birthyear',
-        stem: 'What is your birth year?',
-        answers: _.range((new Date()).getFullYear()-10, 1910, -1) // use underscore to create an array of years from ten years ago back to 1910
-    });
+        stem: 'What is your birth year?',});
 
     API.addQuestionsSet('raceomb',{
         inherit: 'singleChoicedrop',
