799458c8640447203ea44c47a1710c7d962ee297
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-06-07 20:08:23 -0700

    Update raceiat_qualtrics.js

--- raceiat_qualtrics.js
+++ raceiat_qualtrics.js
@@ -40,7 +40,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
         },
 
 		base_url : {//Where are your images at?
-			image : 'https://andrewt257.github.io/test2iat/images/'
+			image : 'https://github.com/andrewt257/test2iat/tree/main/images'
 		} 
 	});
 });
