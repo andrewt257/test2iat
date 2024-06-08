817c11a66c7fc2f14de41d9310768bb6ad29b72c
andrewt257 <andrew.tong752@myci.csuci.edu>
2024-06-07 19:42:20 -0700

    Update raceiat_qualitrics.js

--- raceiat_qualitrics.js
+++ raceiat_qualitrics.js
@@ -3,40 +3,38 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 
 	return iatExtension({
 		category1 : {
-			name : 'Black people', //Will appear in the data.
-			title : {
-				media : {word : 'Black people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image: 'bm1_nc.jpg'},
+            name : global.blackLabels, //Will appear in the data.
+            title : {
+                media : {word : global.blackLabels}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'bm1_nc.jpg'},
                 {image: 'bm2_nc.jpg'},
                 {image: 'bm3_nc.jpg'},
                 {image: 'bf1_nc.jpg'},
                 {image: 'bf2_nc.jpg'},                 
-                {image: 'bf3_nc.jpg'} 
-			], 
-			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		category2 :	{
-			name : 'White people', //Will appear in the data.
-			title : {
-				media : {word : 'White people'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image: 'wm1_nc.jpg'},
+                {image: 'bf3_nc.jpg'}     
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },	
+		category2 : {
+            name : global.whiteLabels, //Will appear in the data.
+            title : {
+                media : {word : global.whiteLabels}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wm1_nc.jpg'},
                 {image: 'wm2_nc.jpg'},
                 {image: 'wm3_nc.jpg'},
                 {image: 'wf1_nc.jpg'},
                 {image: 'wf2_nc.jpg'},
-                {image: 'wf3_nc.jpg'}			], 
-			//Stimulus css
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
+                {image: 'wf3_nc.jpg'}
+            ]},
 
 		base_url : {//Where are your images at?
 			image : 'https://andrewt257.github.io/minno-tasks/images/'
